247c6952be1e97a8be20b3f21864d17a1f9a1fc2
Damian0401 <szkola_abc@o2.pl>
2022-12-04 20:21:55 +0100

    Added day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of code 2022
+
+Click [here](https://adventofcode.com/) for more information.
---
+++ day-01/Program.cs
@@ -0,0 +1,57 @@
+﻿
+Console.WriteLine("Have a nice day!");
+
+// PartOne();
+// PartTwo();
+
+
+void PartOne()
+{
+    var data = File.ReadAllText("data.txt");
+
+    var groupedData = data.Split("\r\n\r\n")
+        .Select(x => x.Split("\n"));
+
+    var parsetData = groupedData
+        .Select(x => 
+            x.Select(y => int.Parse(y)));
+
+    var result = parsetData
+        .Select(x => x.Sum())
+        .Max();
+
+    File.WriteAllText("result.txt", result.ToString());
+}
+
+void PartTwo()
+{
+    var data = File.ReadAllText("data.txt");
+
+    var groupedData = data.Split("\r\n\r\n")
+        .Select(x => x.Split("\n"));
+
+    var parsetData = groupedData
+        .Select(x => 
+            x.Select(y => int.Parse(y)));
+
+    var result = parsetData
+        .Select(x => x.Sum())
+        .OrderByDescending(x => x)
+        .Take(3)
+        .Sum();
+
+    File.WriteAllText("result.txt", result.ToString());
+}
+
+// PartOne
+// var result = data.Split("\r\n\r\n")
+//     .Select(x => x.Split("\n")
+//         .Select(y => int.Parse(y))
+//             .Sum()).Max();
+
+// PartTwo
+// var result = data.Split("\r\n\r\n")
+//     .Select(x => x.Split("\n")
+//         .Select(y => int.Parse(y)).Sum())
+//     .OrderByDescending(x => x)
+//     .Take(3).Sum();
