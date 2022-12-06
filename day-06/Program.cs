8cccc0cf940380b22b8f29bdc77de141f156b194
Damian0401 <szkola_abc@o2.pl>
2022-12-06 14:52:07 +0100

    Added day 6

---
+++ day-06/Program.cs
@@ -0,0 +1,45 @@
+﻿
+Console.WriteLine("Don't worry, be happy!");
+
+// PartOne();
+// PartTwo();
+
+#pragma warning disable
+
+void PartOne()
+{
+    var data = File.ReadAllText("data.txt");
+
+    int codeLength = 4;
+
+    for (int i = codeLength; i < data.Length; i++)
+    {
+        var uniqueElements = data[(i - codeLength)..i]
+            .Distinct();
+
+        if (uniqueElements.Count() == codeLength)
+        {
+            File.WriteAllText("result.txt", i.ToString());
+            break;
+        }
+    }
+}
+
+void PartTwo()
+{
+    var data = File.ReadAllText("data.txt");
+
+    int codeLength = 14;
+
+    for (int i = codeLength; i < data.Length; i++)
+    {
+        var uniqueElements = data[(i - codeLength)..i]
+            .Distinct();
+
+        if (uniqueElements.Count() == codeLength)
+        {
+            File.WriteAllText("result.txt", i.ToString());
+            break;
+        }
+    }
+}
